64715a108bd787c9f4ea981de6c050c82e286e31
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-09-10 15:32:01 +0300

    feat: manual implementation of methods to be generated

--- Src/TestApp/Model/Person.cs
+++ Src/TestApp/Model/Person.cs
@@ -1,21 +1,24 @@
 ﻿using Gedaq.Provider.Attributes;
+using Gedaq.Provider.Attributes.Constraints;
+using ColumnAttribute = Gedaq.Npgsql.Attributes.ColumnAttribute;
 
 namespace TestApp.Model
 {
     [Table("person")]
     public class Person
     {
-        [PK()]
-        [Column("id")]
+        [PK("id_pk_constrain")]
+        [Column("id", NpgsqlTypes.NpgsqlDbType.Integer)]
         public int Id { get; set; }
 
-        [Column("firstname")]
+        [NotNull("firstname_not_null_constrain")]
+        [Column("firstname", NpgsqlTypes.NpgsqlDbType.Text)]
         public string FirstName { get; set; }
 
-        [Column("middlename")]
+        [Column("middlename", NpgsqlTypes.NpgsqlDbType.Text)]
         public string MiddleName { get; set; }
 
-        [Column("lastname")]
+        [Column("lastname", NpgsqlTypes.NpgsqlDbType.Text)]
         public string LastName { get; set; }
     }
 }
--- Src/TestApp/Program.cs
+++ Src/TestApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using Gedaq.Provider.Attributes;
+﻿using Gedaq.Npgsql;
+using Gedaq.Provider.Attributes;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +13,21 @@ namespace TestApp
         static void Main(string[] args)
         {
             var sc = new SomeClass();
-            var persons = sc.Method1();
-            for (int i = 0; i < persons.Count; i++)
+            using (var connection = new NpgsqlConnection(""))
             {
-                Console.WriteLine(@$"
+                connection.Open();
+                //var command = sc.Method1CommandManual(in connection);
+                //var persons = sc.Method1Manual(in command, true);
+
+                var persons = sc.Method1Manual(in connection);
+                for (int i = 0; i < persons.Count; i++)
+                {
+                    Console.WriteLine(@$"
 {nameof(Person.FirstName)}: {persons[i].FirstName}
 {nameof(Person.MiddleName)}: {persons[i].MiddleName}
 {nameof(Person.LastName)}: {persons[i].LastName}
 ");
+                }
             }
         }
     }
@@ -34,18 +43,74 @@ namespace TestApp
             return request?.Query<Person>("M1").ToList();
             //return GedaqSomeClass.M1();
         }
-    }
 
-    [UseProvider]
-    public partial class SomeClass2
-    {
-        private readonly PostgresRequest request;
-
-        [UseProvider]
-        public List<Person> Method1()
+        public NpgsqlCommand Method1CommandManual(in NpgsqlConnection connection)
         {
-            return request?.Query<Person>("M1").ToList();
-            //return M1();
+            var command = connection.CreateCommand();
+            command.CommandText = @$"
+SELECT 
+    p1.id,
+    p1.firstname,
+    p1.middlename,
+    p1.lastname
+FROM person p1
+";
+            return command;
+        }
+
+        public List<Person> Method1ExecuteManual(in NpgsqlCommand command)
+        {
+            using (var reader = command.ExecuteReader())
+            {
+                var persons = new List<Person>();
+                while (reader.Read())
+                {
+                    var item = new Person
+                    {
+                        Id = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        MiddleName = reader.GetString(2),
+                        LastName = reader.GetString(3)
+                    };
+                    persons.Add(item);
+                }
+
+                return persons;
+            }
+        }
+
+        public List<Person> Method1Manual(in NpgsqlConnection connection)
+        {
+            using (var command = Method1CommandManual(in connection))
+            {
+                return Method1ExecuteManual(in command);
+            }
+        }
+
+        public List<Person> Method1Manual(
+            in NpgsqlCommand command,
+            in bool prepareCommand
+            )
+        {
+            if(prepareCommand && !command.IsPrepared)
+            {
+                command.Prepare();
+            }
+
+            return Method1ExecuteManual(in command);
         }
     }
+
+    //[UseProvider]
+    //public partial class SomeClass2
+    //{
+    //    private readonly PostgresRequest request;
+
+    //    [UseProvider]
+    //    public List<Person> Method1()
+    //    {
+    //        return request?.Query<Person>("M1").ToList();
+    //        //return M1();
+    //    }
+    //}
 }
