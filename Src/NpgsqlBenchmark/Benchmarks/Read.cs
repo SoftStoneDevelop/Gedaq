0d22f2cd557968c504e399281a370dfc01ff9ee6
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-02-10 05:25:39 +0300

    feat: read bench

---
+++ Src/NpgsqlBenchmark/Benchmarks/Read.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.IO;
+using NpgsqlBenchmark.Model;
+using Dapper;
+using System.Linq;
+
+namespace NpgsqlBenchmark.Benchmarks
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [HideColumns("Error", "StdDev", "Median", "RatioSD")]
+    public class Read
+    {
+        [Params(10, 20, 30, 40)]
+        public int Size;
+
+        private NpgsqlConnection _connection;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var root = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json", optional: false)
+                .Build()
+                ;
+
+            _connection = new NpgsqlConnection(root.GetConnectionString("SqlConnection"));
+            _connection.Open();
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _connection?.Dispose();
+        }
+
+        [Benchmark(Description = $"Gedaq")]
+        public void Gedaq()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                var persons = _connection.GetAllPerson().ToList();
+            }
+        }
+
+        [Benchmark(Baseline = true, Description = "Dapper.Query<T>")]
+        public void Dapper()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                var persons = _connection.Query<Person>(@"
+SELECT 
+    p1.id,
+    p1.firstname,
+    p1.middlename,
+    p1.lastname
+FROM person p1
+")
+                    .ToList();
+            }
+        }
+    }
+}
