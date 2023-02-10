456d5833c449377ccbf1b036f1ce44343e7470a4
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-02-10 04:50:51 +0300

    feat: add benchmarks

---
+++ Src/NpgsqlBenchmark/Model/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace NpgsqlBenchmark.Model
+{
+    [Gedaq.Npgsql.Attributes.QueryRead(
+            @"
+SELECT 
+    p1.id,
+    p1.firstname,
+    p1.middlename,
+    p1.lastname
+FROM person p1
+",
+            typeof(Person),
+            Gedaq.Provider.Enums.MethodType.Sync,
+            Gedaq.Npgsql.Enums.SourceType.Connection,
+            "GetAllPerson"
+            )]
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Src/NpgsqlBenchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using NpgsqlBenchmark.Benchmarks;
+
+namespace NpgsqlBenchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Read>();
+        }
+    }
+}
