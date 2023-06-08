1c4bf21a404ca6896dfedff879dd1bd4be8e9cac
Vyacheslav <softstonedevelop@gmail.com>
2023-06-08 09:19:43 +0300

    fix: generate test
    #17

---
+++ Src/TestsGenegator/Generators/InsertModelInnerTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using TestsGenegator.Enums;
+
+namespace TestsGenegator.Generators
+{
+    internal static class InsertModelInnerTest
+    {
+        public static void Generate(
+            int order,
+            StringBuilder stringBuilder,
+            Model.Model model, 
+            Database database
+            )
+        {
+            switch (database)
+            {
+                case Database.PostgreSQL:
+                {
+                    stringBuilder.AppendLine($@"
+[Query(
+            query: @""
+INSERT INTO public.{model.ModelInner.TableName}(
+	{model.ModelInner.IdColumnName},
+    {model.ModelInner.ValueColumnName},
+    {model.ModelInner.NullableValueColumnName}
+)
+VALUES (
+    $1, 
+    $2, 
+    $3
+);
+"",
+            methodName:""Insert"",
+            queryMapType: null,
+            methodType: MethodType.Async | MethodType.Sync,
+            sourceType: SourceType.Connection,
+            queryType: QueryType.NonQuery,
+            generate: true,
+            accessModifier: AccessModifier.Private
+            ), 
+            Parametr(typeof({model.ModelInner.IdType}), position: 1, methodParametrName: ""{model.ModelInner.IdColumnName}""),
+            Parametr(typeof({model.ModelInner.ValueType}), position: 2, methodParametrName: ""{model.ModelInner.ValueColumnName}""),
+            Parametr(typeof({model.ModelInner.NullableValueType}), position: 3, methodParametrName: ""{model.ModelInner.NullableValueColumnName}"")
+            ]
+        [Test, Order({order})]
+        public async Task InsertModelInnerTest()
+        {{
+            await using (var connection = GlobalSetUp.GetConnection)
+            {{
+                await connection.OpenAsync();
+                //Insert(connection, 1, 1, null);
+            }}
+        }}
+");
+                    return;
+                }
+                case Database.MsSQL:
+                {
+                    return;
+                }
+                case Database.MySQL:
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Src/TestsGenegator/Generators/TestsPart.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Text;
+using TestsGenegator.Enums;
+using TestsGenegator.Helpers;
+
+namespace TestsGenegator.Generators
+{
+    internal class TestsPart
+    {
+        private readonly StringBuilder _stringBuilder = new StringBuilder();
+
+        public void Generate(Model.Model model, Database database, SourceProductionContext context)
+        {
+            _stringBuilder.Clear();
+            Start(model, database);
+
+            InsertModelInnerTest.Generate(0, _stringBuilder, model, database);
+
+            End();
+
+            context.AddSource($"{model.ClassName}TestsPart.g.cs", _stringBuilder.ToString());
+        }
+
+        private void Start(Model.Model model, Database database)
+        {
+            _stringBuilder.AppendLine($@"
+{database.ToUsings()}
+
+using NUnit.Framework;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Tests
+{{
+    internal partial class Tests{model.TypeInfo.TypeName}
+    {{
+");
+        }
+
+        private void End()
+        {
+            _stringBuilder.AppendLine($@"
+    }}
+}}
+");
+        }
+    }
+}
