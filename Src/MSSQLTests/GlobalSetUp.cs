de6305bae684e2ac763539bec82f8f08b2e2c13a
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2024-09-27 01:09:14 +0300

    feat: + TestContainer MySQL + TestContainer MsSQL

--- Src/MSSQLTests/GlobalSetUp.cs
+++ Src/MSSQLTests/GlobalSetUp.cs
@@ -1,40 +1,42 @@
 using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using System.Data.Common;
-using System.IO;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace Tests
 {
     [SetUpFixture]
     public class GlobalSetUp
     {
-        private static IConfiguration _configuration;
-
         public static SqlConnection GetConnection
         {
             get
             {
                 var connection = (SqlConnection)SqlClientFactory.Instance.CreateConnection();
-                connection.ConnectionString = _configuration.GetConnectionString("SqlConnection");
+                connection.ConnectionString = _connectionString;
                 return connection;
             }
         }
 
         public static DbConnection GetDbConnection => GetConnection;
 
+        private MsSqlContainer _mssql;
+        private static string _connectionString;
+
         [OneTimeSetUp]
         public async Task OneTimeSetUp()
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false);
-            _configuration = builder.Build();
+            _mssql =
+                new MsSqlBuilder()
+                .WithPassword("dhgvbh73j")
+                .Build();
+
+            await _mssql.StartAsync();
 
             await using (var masterConnection = (SqlConnection)SqlClientFactory.Instance.CreateConnection())
             {
-                masterConnection.ConnectionString = _configuration.GetConnectionString("MasterConnection");
+                masterConnection.ConnectionString = _mssql.GetConnectionString();
                 await masterConnection.OpenAsync();
 
                 await using var createCmd = masterConnection.CreateCommand();
@@ -49,21 +51,30 @@ CREATE DATABASE gedaqtests
 ";
                 createCmd.ExecuteNonQuery();
             }
+
+            var builder = new SqlConnectionStringBuilder(_mssql.GetConnectionString());
+            builder.DataSource = "gedaqtests";
+            _connectionString = builder.ConnectionString;
         }
 
         [OneTimeTearDown]
         public async Task OneTimeTearDown()
         {
-            await using (var masterConnection = (SqlConnection)SqlClientFactory.Instance.CreateConnection())
+            if (_mssql != null)
             {
-                masterConnection.ConnectionString = _configuration.GetConnectionString("MasterConnection");
-                await masterConnection.OpenAsync();
-                await using var command = masterConnection.CreateCommand();
-                command.CommandText = $@"
+                await using (var masterConnection = (SqlConnection)SqlClientFactory.Instance.CreateConnection())
+                {
+                    masterConnection.ConnectionString = _mssql.GetConnectionString();
+                    await masterConnection.OpenAsync();
+                    await using var command = masterConnection.CreateCommand();
+                    command.CommandText = $@"
 ALTER DATABASE gedaqtests SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 DROP DATABASE gedaqtests;
 ";
-                await command.ExecuteNonQueryAsync();
+                    await command.ExecuteNonQueryAsync();
+                }
+
+                await _mssql.DisposeAsync();
             }
         }
     }
